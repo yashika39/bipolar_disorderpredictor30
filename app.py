663d8f10bc1bd666d0ecfd0a08d2a84a6d858b81
yashika39 <yashika.yadav.cseds.2021@miet.ac.in>
2025-08-07 14:35:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import joblib
+from pathlib import Path
 from preprocess import clean_text
 
 st.title("Bipolar Disorder Predictor from Clinical Interview")
@@ -8,11 +9,13 @@ user_input = st.text_area("Paste a clinical interview transcript:")
 
 if st.button("Predict"):
     if user_input:
-        vectorizer = joblib.load("../models/vectorizer.pkl")
-        model = joblib.load("../models/LogisticRegression.pkl")  # Choose best model
+        # Load model files from root-based paths
+        vectorizer = joblib.load("models/vectorizer.pkl")
+        model = joblib.load("models/LogisticRegression.pkl")
         cleaned = clean_text(user_input)
         vec_input = vectorizer.transform([cleaned])
         prediction = model.predict(vec_input)[0]
         st.success(f"Prediction: {prediction}")
     else:
         st.warning("Please enter some text.")
+
